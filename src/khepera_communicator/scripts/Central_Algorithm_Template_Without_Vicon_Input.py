f1185a9b38ffd4ab8c24864e0bd769298f611ce2
JSongLaptop <you@example.com>
2021-11-11 14:48:50 -0500

    Initial commit; send and receive over UDP

---
+++ src/khepera_communicator/scripts/Central_Algorithm_Template_Without_Vicon_Input.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# license removed for brevity
+
+import rospy
+import math
+import time
+from std_msgs.msg import String
+# Import message file
+from khepera_communicator.msg import K4_controls, SensorReadings
+
+start = time.time()
+
+def talker():
+	# Set up publisher
+	pub = rospy.Publisher('K4_controls_109', K4_controls, queue_size=10)
+	rospy.init_node('Central_Algo', anonymous=True)
+	
+	# Set publish rate
+	# rate = rospy.Rate(100) # 10hz
+	
+	# Message type
+	msg = K4_controls()
+
+	while not rospy.is_shutdown():
+		
+		end = time.time()
+		t = end - start
+		
+		msg.ctrl_W = 0
+		msg.ctrl_W = 0
+		msg.ctrl_V = 200 * math.sin(3.14159 * t) 
+		msg.ctrl_V = 0
+
+		#rospy.loginfo(msg)
+		pub.publish(msg)
+		#rate.sleep()
+
+if __name__ == '__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass
